f59b87d641dbf86216cba13555a76ed0004e019b
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2024-11-26 19:45:36 -0300

    .

---
+++ t.py
@@ -0,0 +1,2 @@
+from pyglet import font
+print(font.have_font('Chicle'))  # This will list all available fonts
--- util/fontes.py
+++ util/fontes.py
@@ -1,5 +1,8 @@
 from customtkinter import CTkFont
+from pyglet import font
 
+font.add_file('../src/fonts/Chicle-Regular.ttf')
+f_principal = ('Chicle',48)
 
-def f_principal(): return CTkFont(family='src/fonts/Chicle-Regular.ttf', size=48)
+# def f_principal(): return CTkFont(family='src/fonts/Chicle-Regular.ttf', size=48)
 
