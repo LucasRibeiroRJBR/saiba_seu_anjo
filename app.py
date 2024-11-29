8810ec382e7c171d9383d96a302ee94fdaadcc26
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2024-11-29 19:09:42 -0300

    .

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import customtkinter as ctk
-from pyglet import font
+from pyglet import font,options
+
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__() 
 
-        font.add_file('src/fonts/Chicle-Regular.ttf')
-        self.custom_font = font.load('Chicle')
+        options['win32_gdi_font'] = True
+
+        font.add_file('Chicle-Regular.ttf')
 
         self.geometry('500x300')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
         self.letreiro = ctk.CTkLabel(master=self, text='Descubra seu Anjo', font=('Chicle',48))
+
         self.letreiro.grid(row=0, column=0)
 
 if __name__ == '__main__':
