11bacdddd9230c3439b4ed347777b2e06a73deab
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2024-11-29 18:59:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import customtkinter as ctk
-import tkinter
+from pyglet import font
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__() 
 
-        #self.ctk_font = ctk.CTkFont(family=custom_font, size=20, weight="bold")       
-        self.custom_font = tkinter.font.Font(family="Chicle", size=14, file='src/fonts/Chicle-Regular.ttf')
+        font.add_file('src/fonts/Chicle-Regular.ttf')
+        self.custom_font = font.load('Chicle')
 
         self.geometry('500x300')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
-        self.letreiro = ctk.CTkLabel(master=self, text='Descubra seu Anjo', font=self.custom_font)
+        self.letreiro = ctk.CTkLabel(master=self, text='Descubra seu Anjo', font=('Chicle',48))
         self.letreiro.grid(row=0, column=0)
 
 if __name__ == '__main__':
