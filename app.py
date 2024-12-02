7799b95ce3e594bdf6c4adc31c7dab0c79496cf6
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2024-12-01 23:47:34 -0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from pyglet import font,options
+from util import images as i
 
 
 class Resultado(ctk.CTkToplevel):
@@ -7,10 +8,12 @@ class Resultado(ctk.CTkToplevel):
         super().__init__(parent)
         self.rowconfigure((0,1,2), weight=1)
         self.columnconfigure((0,1), weight=1)
-        self.geometry('400x200')
+        self.geometry('700x500')
         self.after(100, self.lift)
 
-        self.lb_teste = ctk.CTkLabel(master=self,text=f'{variable.get()}',font=('Chicle',48))
+        
+
+        self.lb_teste = ctk.CTkLabel(master=self,text=f'{variable}',image=i.Imamish,font=('Chicle',48))
 
         self.lb_teste.grid(row=0,column=0)
 
@@ -23,7 +26,7 @@ class App(ctk.CTk):
         self.columnconfigure((0,1), weight=1)
         self.toplevel_resul = None
 
-        font.add_file('Chicle-Regular.ttf')
+        font.add_file('src/fonts/Chicle-Regular.ttf')
         self.vDt_nasc = ctk.StringVar()
 
         self.letreiro = ctk.CTkLabel(master=self,text='Descubra seu Anjo',font=('Chicle',48))
@@ -35,13 +38,6 @@ class App(ctk.CTk):
         self.lb_dt_nasc.grid(row=1,column=0,sticky='e',padx=5)
         self.in_dt_nasc.grid(row=1,column=1,sticky='w',padx=5)
         self.bt_show.grid(row=2, column=0, columnspan=2,pady=15)
-
-    def get_info(self):
-        #print(f'{self.vDt_nasc.get()}')
-        if self.toplevel_resul is None or not self.toplevel_resul.winfo_exists():
-            self.toplevel_resul = Resultado(self)
-            self.toplevel_resul.after(0,self.toplevel_resul.lift)
-        else: pass
 
     def open_toplevel(self):
         # Pass the main app variable to Toplevel
---
+++ util/images.py
@@ -0,0 +1,4 @@
+from customtkinter import CTkImage
+from PIL import Image
+
+Imamish = CTkImage(dark_image=Image.open('src/img/Imamiah.png'),size=(275,325))
